a50d30b3db606e117c2e74a7896e978fc9037eb5
Alexey Mikhailyuk <35634279+alexmihalyk23@users.noreply.github.com>
2025-12-13 13:15:42 +0700

    Remove GPSCamera import and update __all__

--- base/__init__.py
+++ base/__init__.py
@@ -1,5 +1,5 @@
 from .camera import Camera
 from .orange import RK3588
-from .rtsp_and_gps import GPSCamera
+#from .rtsp_and_gps import GPSCamera
 
-__all__ = ["Camera", "RK3588", "GPSCamera"]
+__all__ = ["Camera", "RK3588"]
